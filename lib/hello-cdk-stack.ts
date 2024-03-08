35ca3e7041bfa994c2fdb191197c9ca049c4d664
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-03-08 13:38:40 +0000

    add extension prettier

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -3,13 +3,13 @@ import { aws_s3 as s3 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    new s3.Bucket(this, 'MyFirstBucket', {
-      versioned: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
-  }
+        new s3.Bucket(this, 'MyFirstBucket', {
+            versioned: true,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
+    }
 }
